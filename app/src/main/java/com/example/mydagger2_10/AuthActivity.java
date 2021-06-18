1595d149d4f04e9bbe3fa1317cd00303d4f09e20
Dipankar <dip1410@gmail.com>
2021-06-18 23:45:36 +0530

    rx java observable observer simplest model created

--- app/src/main/java/com/example/mydagger2_10/AuthActivity.java
+++ app/src/main/java/com/example/mydagger2_10/AuthActivity.java
@@ -16,7 +16,10 @@ import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -52,31 +55,66 @@ public class AuthActivity extends DaggerAppCompatActivity {
     private void getCountries(AuthApi api) {
         //    Api api = retrofit.create(Api.class);
         Observable<List<Country>> countryObservable = api.getCountries();
-        countryObservable.subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
-                .map(result -> Observable.fromIterable(result))
-                .flatMap(x -> x).filter(y -> {
-            return true;
-        }).toList().toObservable()
-                .subscribe(this::handleResults, this::handleError);
+        countryObservable.subscribeOn(Schedulers.io())//Asynchronously subscribes Observable to perform action in I/O Thread.
+                .observeOn(AndroidSchedulers.mainThread()) // To perform its emissions and response on UiThread(or)MainThread.
+                .subscribe(new Observer<List<Country>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+                    @Override
+                    public void onNext(List<Country> countryList) {
+                        String[] countries = new String[countryList.size()];
+                        if (countryList != null && countryList.size() != 0) {
+                            for (int i = 0; i < countryList.size(); i++) {
+                                countries[i] = countryList.get(i).getName();
+                                Log.d(TAG, countries[i]);
+                            }
+                            //displaying the string array into listview
+                            listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, countries));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "NO RESULTS FOUND",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, e.getMessage());
+                        e.printStackTrace(); // Just to see complete log information. we can comment if not necessary!
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "The service Observable has ended!");
+                    }
+                });
+
+//        countryObservable.subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
+//                .map(result -> Observable.fromIterable(result))
+//                .flatMap(x -> x).filter(y -> {
+//            return true;
+//        }).toList().toObservable()
+//                .subscribe(this::handleResults, this::handleError);
     }
 
-    private void handleResults(List<Country> countryList) {
-        String[] countries = new String[countryList.size()];
-        if (countryList != null && countryList.size() != 0) {
-            for (int i = 0; i < countryList.size(); i++) {
-                countries[i] = countryList.get(i).getName();
-                Log.d(TAG, countries[i]);
-            }
-            //displaying the string array into listview
-            listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, countries));
-        } else {
-            Toast.makeText(this, "NO RESULTS FOUND",
-                    Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void handleError(Throwable t) {
-        Toast.makeText(this, "ERROR IN FETCHING API RESPONSE. Try again",
-                Toast.LENGTH_LONG).show();
-    }
+//    private void handleResults(List<Country> countryList) {
+//        String[] countries = new String[countryList.size()];
+//        if (countryList != null && countryList.size() != 0) {
+//            for (int i = 0; i < countryList.size(); i++) {
+//                countries[i] = countryList.get(i).getName();
+//                Log.d(TAG, countries[i]);
+//            }
+//            //displaying the string array into listview
+//            listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, countries));
+//        } else {
+//            Toast.makeText(this, "NO RESULTS FOUND",
+//                    Toast.LENGTH_LONG).show();
+//        }
+//    }
+//
+//    private void handleError(Throwable t) {
+//        Toast.makeText(this, "ERROR IN FETCHING API RESPONSE. Try again",
+//                Toast.LENGTH_LONG).show();
+//    }
 }
